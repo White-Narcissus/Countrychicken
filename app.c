15c931ba2a0d97b16d4da13363e32ea23b95b671
White_Narcissus <140405197+White-Narcissus@users.noreply.github.com>
2026-04-01 20:12:25 +0800

    Thefirstsubmission

---
+++ app.c
@@ -0,0 +1,30 @@
+/* #include "raylib.h"
+
+int main(void)
+{
+    InitWindow(800, 450, "raylib test");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, raylib!", 190, 200, 20, DARKGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+} */
+
+#include <stdio.h>
+#include "raylib.h"
+
+int main(int argc, char *argv[])
+{
+    // Create game window
+    InitWindow(640, 400, "Adventure King");
+
+    // Create game main cycle
+    return 0;
+}
